c1589e49d84321c969a1ff586cfc581098cab855
MaryAnzh <MaryAnzh@mail.ru>
2022-08-30 01:07:11 +0400

    feat: add burger nav lost animation

--- js/burger.js
+++ js/burger.js
@@ -1,12 +1,26 @@
 class Burger {
     burgerWrap;
     burgerLines;
+    burgerList;
     isBurgerOpen;
+    navList;
+    navListUl;
 
     constructor() {
         this.burgerWrap = document.querySelector('#burger');
         this.burgerLines = this.burgerWrap.children;
+        this.burgerList = document.querySelector('.cv__nav__burger-list');
+        this.navList = [
+            'about',
+            'skill',
+            'projects',
+            'code',
+            'education',
+            'languages'
+        ];
         this.isBurgerOpen = false;
+        this.navListUl = document.createElement('ul');
+        this.navListUl.classList.add('cv__nav__burger-list__list');
     }
 
     burgerOnClick(e) {
@@ -17,6 +31,7 @@ class Burger {
             this.burgerLines[0].style.transform = 'rotate(.125turn)';
             this.burgerLines[2].style.bottom = '21.5px';
             this.burgerLines[2].style.transform = 'rotate(-.125turn)';
+            this.navListUl.style.transform = 'translate(0px, 0px)';
             this.isBurgerOpen = true;
         } else {
             this.burgerWrap.style.borderRadius = '2px';
@@ -25,12 +40,25 @@ class Burger {
             this.burgerLines[0].style.transform = 'rotate(0)';
             this.burgerLines[2].style.bottom = '8px';
             this.burgerLines[2].style.transform = 'rotate(0)';
+            this.navListUl.style.transform = 'translate(0px, -190px)';
             this.isBurgerOpen = false;
 
         }
 
     }
+
+    createBurgerListUl() {
+        this.navList.forEach((elem) => {
+            const li = document.createElement('li');
+            li.classList.add('translator');
+            li.setAttribute('data-translat', `NAV.${elem.toUpperCase()}`);
+            this.navListUl.appendChild(li);
+        });
+        this.burgerList.appendChild(this.navListUl);
+    }
 }
 
 const burger = new Burger();
+burger.createBurgerListUl();
+
 burger.burgerWrap.addEventListener('click', (e) => burger.burgerOnClick(e));
