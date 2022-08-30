59239f709f0e6361342dc5f2b519ed3cc1d1a2c1
MaryAnzh <MaryAnzh@mail.ru>
2022-08-31 00:57:56 +0400

    feat: add skill info

--- js/skill.js
+++ js/skill.js
@@ -1,5 +1,7 @@
 class Skill {
     list = document.querySelector('.cv__main__skill__wrap__lists__texst-list');
+    iconList = document.querySelector('.cv__main__skill__wrap__lists__icons-list');
+
     skillList = [
         'JavaScript, TypeScript',
         'HTML, CSS, SCSS, BEM',
@@ -7,6 +9,17 @@ class Skill {
         'git',
         'WebPack',
         'Adobe Illustrato',
+    ];
+    skillIcons = [
+        'js.png',
+        'ts.png',
+        'html.png',
+        'css.png',
+        'scss.png',
+        'angular.svg',
+        'git.png',
+        'webpack.png',
+        'AI.png'
     ];
 
     constructor() { }
@@ -18,7 +31,18 @@ class Skill {
             this.list.appendChild(li);
         });
     }
+
+    createIconList() {
+        this.skillIcons.forEach((elem) => {
+            const img = document.createElement('img');
+            img.setAttribute('src', `assets/icons/${elem}`);
+            img.setAttribute('alt', `${elem.split('.')[0]}`);
+            img.classList.add('cv__main__skill__wrap__lists__icons-list__icon');
+            this.iconList.appendChild(img);
+        });
+    }
 }
 
 const skill = new Skill;
 skill.createSkillList();
+skill.createIconList();
