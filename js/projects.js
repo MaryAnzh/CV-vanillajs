70f7d98dd259a73be3a7637ef2a9aff8448f79c0
MaryAnzh <MaryAnzh@mail.ru>
2022-09-01 17:48:59 +0400

    feat: add slider

--- js/projects.js
+++ js/projects.js
@@ -1,6 +1,8 @@
 class Projects {
     slider_HTMLElem = document.querySelector('.cv__main__projects__wrap__slider-wrap__slider');
     projectsList_HTMLElem = document.querySelector('.cv__main__projects__wrap__list-wrap__list');
+    leftSloderArroy_HTMLElem = document.querySelector('#leftSliderArroy');
+    rightSloderArroy_HTMLElem = document.querySelector('#rightSliderArroy');
 
     projects = [
         {
@@ -21,11 +23,26 @@ class Projects {
         },
     ];
 
+    projectsLenth = this.projects.length;
+
+    sliderWidth = 50 * this.projectsLenth;
+    shiftCount = 0;
+    shiftCountMin = 0;
+    shiftCountMax = this.projectsLenth - 2;
+    shiftSize = 200 / this.projectsLenth;
+    currentShift = 0;
+
+
     constructor() {
         this.currentShowList = [
             this.projects[0],
             this.projects[1],
         ];
+        this.createProjectsList();
+        this.createSliderItem();
+
+        this.leftSloderArroy_HTMLElem.addEventListener('click', (e) => this.shiftSliderItemOnClick(e));
+        this.rightSloderArroy_HTMLElem.addEventListener('click', (e) => this.shiftSliderItemOnClick(e));
     }
 
     createSliderItem() {
@@ -41,6 +58,7 @@ class Projects {
         </div>`;
             htmlNodes += htmlElem;
         });
+        this.slider_HTMLElem.style.width = `${this.sliderWidth}%`;
         this.slider_HTMLElem.innerHTML = htmlNodes;
     }
 
@@ -51,8 +69,40 @@ class Projects {
             this.projectsList_HTMLElem.appendChild(li);
         });
     }
+
+    shiftSliderItemOnClick(e) {
+        const arrow = e.target;
+        const type = arrow.dataset.type;
+
+        if (type === 'left') {
+            this.shiftCount++;
+            this.currentShift -= this.shiftSize;
+            console.log('shiftCount');
+            console.log(this.shiftCount);
+            if (this.shiftCount === this.shiftCountMax) {
+                this.leftSloderArroy_HTMLElem.classList.add('arrow-block');
+            }
+            if (this.shiftCount === this.shiftCountMin + 1) {
+                console.log('отработал');
+                this.rightSloderArroy_HTMLElem.classList.remove('arrow-block');
+            }
+        }
+        if (type === 'right') {
+            this.shiftCount--;
+            this.currentShift += this.shiftSize;
+
+            if (this.shiftCount === this.shiftCountMin) {
+                this.rightSloderArroy_HTMLElem.classList.add('arrow-block');
+            }
+            if (this.shiftCount === this.shiftCountMax - 1) {
+                this.leftSloderArroy_HTMLElem.classList.remove('arrow-block');
+            }
+        }
+
+        this.slider_HTMLElem.style.marginLeft = `${this.currentShift}%`
+
+    }
+
 }
 
 const projects = new Projects();
-projects.createSliderItem();
-projects.createProjectsList();
