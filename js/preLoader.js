69a4dcc91609d0d326fda94cd0f1ee5eb5f1e7bb
MaryAnzh <MaryAnzh@mail.ru>
2022-09-07 00:54:33 +0400

    feat: add animation round on preloader

--- js/preLoader.js
+++ js/preLoader.js
@@ -1,7 +1,40 @@
 class PreLoader {
     preloaderHTML = document.querySelector('.cv__preloader-wrap');
+    round = document.querySelector('.cv__preloader-wrap__photo');
 
-    constructor() { }
+    constructor() {
+        this.render();
+    }
+
+    render() {
+        const ballLine = document.createElement('ul');
+        ballLine.classList.add('ball-line');
+
+        const addBallOnRound = (itemCount, radius) => {
+            // центр окружности
+            const xCenter = 145;
+            const yCenter = 145;
+
+            // создаем заданное количество шариков
+            for (let index = 0; index < itemCount; index++) {
+                const fairyLight = document.createElement('li');
+                fairyLight.classList.add('ball-line__ball');
+
+                //высчитываем радианы
+                let radian = -(((Math.PI * 2) * (index / itemCount)) - (Math.PI + Math.PI / 20));
+                let x = xCenter + radius * Math.cos(radian);
+                let y = yCenter + radius * Math.sin(radian);
+
+                fairyLight.style.marginTop = `${x}px`;
+                fairyLight.style.marginLeft = `${y}px`;
+                setTimeout(() => ballLine.appendChild(fairyLight), index * 30);
+
+            }
+            this.round.appendChild(ballLine);
+        }
+        addBallOnRound(30, 130);
+    }
+
 
     onLoad(e) {
         setTimeout(() => {
