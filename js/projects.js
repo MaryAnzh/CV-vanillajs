113303b87337f9a36f2d54fe9863cac939344af0
MaryAnzh <maryang@mail.ru>
2022-09-09 23:02:27 +0400

    fix: projects style

--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,7 @@
 class Projects {
     slider_HTMLElem = document.querySelector('.cv__main__projects__wrap__slider-wrap__slider');
     sliderItem__ = document.querySelector('.cv__main__projects__wrap__slider-wrap__slider__item');
-    projectsList_HTMLElem = document.querySelector('.cv__main__projects__wrap__list-wrap__list');
+    projectsList = document.querySelector('.cv__main__projects__wrap__body__list-wrap__list');
     leftSloderArroy_HTMLElem = document.querySelector('#leftSliderArroy');
     rightSloderArroy_HTMLElem = document.querySelector('#rightSliderArroy');
 
@@ -54,33 +54,33 @@ class Projects {
 
         //rendering
         this.renderProjectsList();
-        this.renderSliderItem();
+        //this.renderSliderItem();
 
         //EventListener
-        this.leftSloderArroy_HTMLElem.addEventListener('click', (e) => this.shiftSliderItemOnClick(e));
-        this.rightSloderArroy_HTMLElem.addEventListener('click', (e) => this.shiftSliderItemOnClick(e));
-        window.addEventListener('resize', (e) => this.updateSloderView(e));
+        // this.leftSloderArroy_HTMLElem.addEventListener('click', (e) => this.shiftSliderItemOnClick(e));
+        // this.rightSloderArroy_HTMLElem.addEventListener('click', (e) => this.shiftSliderItemOnClick(e));
+        // window.addEventListener('resize', (e) => this.updateSloderView(e));
     }
 
-    renderSliderItem() {
-        this.projects.forEach((elem, index) => {
-            const div = document.createElement('div');
-            div.classList.add('cv__main__projects__wrap__slider-wrap__slider__item');
-            const htmlElem =
-                `<div class="cv__main__projects__wrap__slider-wrap__slider__item__item-view">
-                <div class="img-wrqp">
-                  <div class="item-number">${index + 1}</div>
-                  <img
-                    class="cv__main__projects__wrap__slider-wrap__slider__item__item-view__img"
-                    src="assets/projects/${elem.src}"
-                    alt="${elem.name}">
-                </div>
-                <p class="cv__main__projects__wrap__slider-wrap__slider__item__item-view__title">${elem.name}</p>
-            </div>`;
-            div.innerHTML = htmlElem;
-            this.slider_HTMLElem.appendChild(div);
-        });
-    }
+    // renderSliderItem() {
+    //     this.projects.forEach((elem, index) => {
+    //         const div = document.createElement('div');
+    //         div.classList.add('cv__main__projects__wrap__slider-wrap__slider__item');
+    //         const htmlElem =
+    //             `<div class="cv__main__projects__wrap__slider-wrap__slider__item__item-view">
+    //             <div class="img-wrqp">
+    //               <div class="item-number">${index + 1}</div>
+    //               <img
+    //                 class="cv__main__projects__wrap__slider-wrap__slider__item__item-view__img"
+    //                 src="assets/projects/${elem.src}"
+    //                 alt="${elem.name}">
+    //             </div>
+    //             <p class="cv__main__projects__wrap__slider-wrap__slider__item__item-view__title">${elem.name}</p>
+    //         </div>`;
+    //         div.innerHTML = htmlElem;
+    //         this.slider_HTMLElem.appendChild(div);
+    //     });
+    // }
 
     renderProjectsList() {
         this.projects.forEach((proj) => {
@@ -89,65 +89,65 @@ class Projects {
             <p>${proj.name} ${deploy} --> <a href="${proj.urlRepo}" target="_blank"> Repo </a></p>
             `;
             li.innerHTML = liContent;
-            this.projectsList_HTMLElem.appendChild(li);
+            this.projectsList.appendChild(li);
         });
     }
 
-    updateSloderView() {
-        this.windowInnerWidth = window.innerWidth;
-        this.currwntViewItem = this.windowInnerWidth > this.miniSliderScreenWidth ? 2 : 1;
+    // updateSloderView() {
+    //     this.windowInnerWidth = window.innerWidth;
+    //     this.currwntViewItem = this.windowInnerWidth > this.miniSliderScreenWidth ? 2 : 1;
 
-        const viewItem = this.currwntViewItem;
-        const min = 0;
-        const max = this.projectsLenth - viewItem;
-        const items = document.querySelectorAll('.cv__main__projects__wrap__slider-wrap__slider__item');
+    //     const viewItem = this.currwntViewItem;
+    //     const min = 0;
+    //     const max = this.projectsLenth - viewItem;
+    //     const items = document.querySelectorAll('.cv__main__projects__wrap__slider-wrap__slider__item');
 
-        if (this.shiftCount === min) {
-            this.rightSloderArroy_HTMLElem.classList.add('arrow-block');
-        }
-        if (this.shiftCount === max) {
-            this.leftSloderArroy_HTMLElem.classList.add('arrow-block');
-        }
-        if (this.shiftCount === min + 1) {
-            this.rightSloderArroy_HTMLElem.classList.remove('arrow-block');
-        }
-        if (this.shiftCount === max - 1) {
-            this.leftSloderArroy_HTMLElem.classList.remove('arrow-block');
-        }
-        if (this.shiftCount > max) {
-            items.forEach((elem) => {
-                elem.style.transform = `translateX(${this.currentShift + 100}%)`;
-            });
-            this.shiftCount--;
-            this.currentShift += 100;
-        }
-    }
+    //     if (this.shiftCount === min) {
+    //         this.rightSloderArroy_HTMLElem.classList.add('arrow-block');
+    //     }
+    //     if (this.shiftCount === max) {
+    //         this.leftSloderArroy_HTMLElem.classList.add('arrow-block');
+    //     }
+    //     if (this.shiftCount === min + 1) {
+    //         this.rightSloderArroy_HTMLElem.classList.remove('arrow-block');
+    //     }
+    //     if (this.shiftCount === max - 1) {
+    //         this.leftSloderArroy_HTMLElem.classList.remove('arrow-block');
+    //     }
+    //     if (this.shiftCount > max) {
+    //         items.forEach((elem) => {
+    //             elem.style.transform = `translateX(${this.currentShift + 100}%)`;
+    //         });
+    //         this.shiftCount--;
+    //         this.currentShift += 100;
+    //     }
+    // }
 
-    shiftSliderItemOnClick(e) {
-        const arrow = e.target;
-        const type = arrow.dataset.type;
+    // shiftSliderItemOnClick(e) {
+    //     const arrow = e.target;
+    //     const type = arrow.dataset.type;
 
-        switch (type) {
-            case 'left':
-                this.shiftCount++;
-                this.currentShift -= 100;
-                break;
+    //     switch (type) {
+    //         case 'left':
+    //             this.shiftCount++;
+    //             this.currentShift -= 100;
+    //             break;
 
-            case 'right':
-                this.shiftCount--;
-                this.currentShift += 100;
-                break;
+    //         case 'right':
+    //             this.shiftCount--;
+    //             this.currentShift += 100;
+    //             break;
 
-            default:
-                break;
-        }
+    //         default:
+    //             break;
+    //     }
 
-        const items = document.querySelectorAll('.cv__main__projects__wrap__slider-wrap__slider__item');
-        items.forEach((elem) => {
-            elem.style.transform = `translateX(${this.currentShift}%)`;
-        });
-        this.updateSloderView();
-    }
+    //     const items = document.querySelectorAll('.cv__main__projects__wrap__slider-wrap__slider__item');
+    //     items.forEach((elem) => {
+    //         elem.style.transform = `translateX(${this.currentShift}%)`;
+    //     });
+    //     this.updateSloderView();
+    // }
 }
 
 const projects = new Projects();
